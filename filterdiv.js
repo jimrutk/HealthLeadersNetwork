ae67af2163bbda4274f764b6a1ccffa9387eac44
jimrutk <109818684+jimrutk@users.noreply.github.com>
2022-07-23 00:40:35 -0400

    Update filterdiv.js

--- filterdiv.js
+++ filterdiv.js
@@ -1,4 +1,5 @@
 filterSelection("all")
+activeCurrentButton()
 
 function filterSelection(c) {
   var x, i;
@@ -8,7 +9,6 @@ function filterSelection(c) {
     w3RemoveClass(x[i], "hcb-show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "hcb-show");
   }
-  activeCurrentButton();
 }
 
 function w3AddClass(element, name) {
