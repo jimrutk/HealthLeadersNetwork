14d73bcbccf4b6e4cbab0715f697cb48c08e353c
jimrutk <109818684+jimrutk@users.noreply.github.com>
2022-07-23 21:32:03 -0400

    Update and rename filterdiv_7-23-22.js to filterdiv_7-24-22.js

--- filterdiv_7-24-22.js
+++ filterdiv_7-24-22.js
@@ -35,8 +35,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 function activeCurrentButton() {
-  var btnContainer = document.getElementById("hcb-myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("hcb-btn");
+  var btns = document.getElementsByClassName("hcb-btn");
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function(){
       var current = document.getElementsByClassName("hcb-active");
