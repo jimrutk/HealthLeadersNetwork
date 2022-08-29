7d933544f00956a35a0eb5867a256f9f13f73255
jimrutk <109818684+jimrutk@users.noreply.github.com>
2022-08-29 15:54:41 -0400

    Update and rename hcbStickyPageMenu5.js to hcbStickyPageMenu6.js

--- hcbStickyPageMenu6.js
+++ hcbStickyPageMenu6.js
@@ -4,7 +4,7 @@ window.onscroll = function() {hcbStickyMenu()};
 
 /* Find the bottom of the sticky menu that is already on the page */
 /* Problem is this is element in HB, different name when public vs private */
-var hcbPageHeader = document.getElementById("header-view");
+var hcbPageHeader = document.getElementById("NEED CORRECT OBJECT NAME");
 var hcbStickyStart = hcbPageHeader.offsetHeight;
 /* Set up a string so can set the right location for top of the sticky nav menu */
 var hcbSuffix = "px";
@@ -13,11 +13,11 @@ var hcbTopUpdate = hcbStickyStart + hcbSuffix;
 /* Determine the amount of scrolling at which point the floating nav should be made sticky. 
 This is distance from start position of nav bar to bottom of the page's sticky menu */
 var hcbnavbar = document.getElementById("hcb-pspagemenu");
-var hcbSticky = navbar.offsetTop - hcbStickyStart;
+var hcbStickyOffset = hcbnavbar.offsetTop - hcbStickyStart;
 
 
 function hcbStickyMenu() {
-  if (window.pageYOffset >= hcbSticky) {
+  if (window.pageYOffset >= hcbStickyOffset) {
     hcbnavbar.classList.add("hcb-sticky")
     hcbnavbar.style.top = hcbTopUpdate;
 
